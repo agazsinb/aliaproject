1ec4eed957bca528204f846219d3273ff7052658
agazsibahasoean <agazsi.bahasoean@gmail.com>
2017-01-20 19:46:31 +0700

    prgs 03

--- template/js/main.js
+++ template/js/main.js
@@ -128,32 +128,9 @@ jQuery(function($) {
 		});
 	});
 
-	//Error message for form (number only)
-	$(document).ready(function () {
-	  //called when key is pressed in textbox
-	  $("#numbonly").keypress(function (e) {
-	     //if the letter is not digit then display error and don't type anything
-	     if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
-	        //display error message
-	        $("#errmsg").html("Digits Only").show().fadeOut("slow");
-	               return false;
-	    }
-	   });
-	});
-
-	$(document).ready(function () {
-	  //called when key is pressed in textbox
-	  $("#numbonly2").keypress(function (e) {
-	     //if the letter is not digit then display error and don't type anything
-	     if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
-	        //display error message
-	        $("#errmsg2").html("Digits Only").show().fadeOut("slow");
-	               return false;
-	    }
-	   });
-	});
-
 	//DATE TIME PICKER
+
+
 
 	//Google Map
 	var latitude = $('#google-map').data('latitude')
