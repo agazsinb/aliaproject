003606c0d8772a76716480c28bf25ba16c78613a
Kitto Adinatha <kitto.adinatha@gmail.com>
2017-01-31 14:52:01 +0700

    connected some web pages email working and is looking gooooood

---
+++ Alia/resources/views/template_fix/meetingresvemail.blade.php
@@ -0,0 +1,22 @@
+
+	<html>
+	<body>
+
+	EVENT INFORMATION <br>
+	Event Name: Birthday<br>
+	Event Purpose: Birthday<br>
+	Meeting Room Selection: 1<br>
+	Estimated Number Of Attendees: 1<br>
+	Estimated Guest Room Count: 1<br>
+	Event Start Dates: 01/31/2017<br>
+	Event End Dates: 02/01/2017<br><br>
+
+	CONTACT INFORMATION<br>
+	First Name: Agazsi<br>
+	Last Name: Bahasoean@wtf.com<br>
+	Phone Number: 08181818<br>
+	Email Address: aji@gmail.com<br>
+	Company/Association: Fasilkom UI
+
+	</body>
+	</html>
